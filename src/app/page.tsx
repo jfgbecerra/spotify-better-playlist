eec8ba32413df9e49095fc0aabde5ec53addbc39
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2023-07-27 20:08:42 -0500

    Added next auth with zustand for state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,24 @@
-"use client"
-import { useEffect } from "react";
+import {
+  LoginButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/components/buttons.component";
 
 export default function Home() {
-  const clientId = "dcb4cb553d364e78b940d73c3bd66293"; // Replace with your client id
-  const params = new URLSearchParams(window.location.search);
-  const code = params.get("code");
-
-  async function checkForAccessToken() {
-    if (!code) {
-      redirectToAuthCodeFlow(clientId);
-    } else {
-      const accessToken = await getAccessToken(clientId, code);
-      const profile = await fetchProfile(accessToken);
-    }
-  }
-
-  useEffect(() => {
-    checkForAccessToken();
-  });
-
-  async function redirectToAuthCodeFlow(clientId: string) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
-
-    localStorage.setItem("verifier", verifier);
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("response_type", "code");
-    params.append("redirect_uri", "https://jfb0009-scaling-space-enigma-g7xq6p74r572w444-3000.preview.app.github.dev");
-    params.append("scope", "user-read-private user-read-email");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
-
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(length: number) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier: string) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
-
- async function getAccessToken(clientId: string, code: string): Promise<string> {
-  const verifier = localStorage.getItem("verifier");
-
-  const params = new URLSearchParams();
-  params.append("client_id", clientId);
-  params.append("grant_type", "authorization_code");
-  params.append("code", code);
-  params.append("redirect_uri", "https://jfb0009-scaling-space-enigma-g7xq6p74r572w444-3000.preview.app.github.dev");
-  params.append("code_verifier", verifier!);
-
-  const result = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: params
-  });
-
-  const { access_token } = await result.json();
-  return access_token;
-}
-
-async function fetchProfile(token: string): Promise<any> {
-  const result = await fetch("https://api.spotify.com/v1/me", {
-      method: "GET", headers: { Authorization: `Bearer ${token}` }
-  });
-
-  return await result.json();
-}
-
-
   return (
-    <>
-      <h1>Display your Spotify profile data</h1>
-
-      <section id="profile">
-        <h2>
-          Logged in as <span id="displayName"></span>
-        </h2>
-        <span id="avatar"></span>
-        <ul>
-          <li>
-            User ID: <span id="id"></span>
-          </li>
-          <li>
-            Email: <span id="email"></span>
-          </li>
-          <li>
-            Spotify URI: <a id="uri" href="#"></a>
-          </li>
-          <li>
-            Link: <a id="url" href="#"></a>
-          </li>
-          <li>
-            Profile Image: <span id="imgUrl"></span>
-          </li>
-        </ul>
-      </section>
-    </>
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+      }}
+    >
+      <div>
+        <LoginButton />
+        <RegisterButton />
+        <ProfileButton />
+      </div>
+    </main>
   );
 }
