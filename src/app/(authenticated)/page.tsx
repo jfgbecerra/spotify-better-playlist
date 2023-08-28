f31e1251ecc75d7afe33133ee76205eb4e18fcf6
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2023-08-28 18:12:13 -0500

    Added middleware for auth protection

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -3,14 +3,12 @@
 import { getSession, signOut, useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  const { data: session } = useSession();
-
+export default function Home() {
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center gap-20'>
       <button
         className='bg-primary flex rounded-full px-12 py-2 text-lg uppercase tracking-widest hover:bg-opacity-80 focus:outline-none'
-        onClick={() => signOut({ callbackUrl: 'http://localhost:3000/login' })}
+        onClick={() => signOut()}
       >
         Log Out
       </button>
