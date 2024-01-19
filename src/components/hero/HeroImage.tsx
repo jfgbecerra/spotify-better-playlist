8deb50f39ce4ee8020adb04c49ced703f5471d96
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2024-01-19 11:33:42 -0600

    Added buffering while site loads for hero image

--- src/components/hero/HeroImage.tsx
+++ src/components/hero/HeroImage.tsx
@@ -5,14 +5,24 @@ import HeroImage from '@/assets/spotify-better-playlist.vercel.app_cropped.png';
 import HeroImageLight from '@/assets/spotify-better-playlist.vercel.app_cropped_light.png';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
+import { Skeleton } from '@nextui-org/react';
 
 export default function HeroImages() {
   const { resolvedTheme } = useTheme();
   const [newTheme, setNewTheme] = useState('dark');
 
+  const [mounted, setMounted] = useState(false);
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   useEffect(() => {
     setNewTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
   }, [resolvedTheme]);
+
+  if (!mounted) return <Skeleton className='h-[1080px] w-[1920px]' />;
 
   if (newTheme === 'dark') {
     return (
