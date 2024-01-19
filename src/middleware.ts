bad960a90ef24fdf49b350d4563d57219674ee20
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2024-01-18 23:06:04 -0600

    Change Middleware to point UnAuth users to about

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from 'next-auth/middleware';
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
   pages: {
-    signIn: '/login',
+    signIn: '/about',
   },
 });
 
