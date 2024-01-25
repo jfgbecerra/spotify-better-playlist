dd2984ba1af2fb27c9a8eb3bd51097a3c99ccd33
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2024-01-24 23:34:23 -0600

    Add play playlist button and remove unused state from play track

--- src/components/buttons/PlayPlaylistButton.tsx
+++ src/components/buttons/PlayPlaylistButton.tsx
@@ -1,27 +1,29 @@
 'use client';
 
-import { playTrack } from '@/lib/playlist-data-accessor';
+import { playPlaylist } from '@/lib/playlist-data-accessor';
 import { AuthSession, Track } from '@/types';
 import { Button } from '@nextui-org/button';
 import { PlayCircle } from 'lucide-react';
 import { useSession } from 'next-auth/react';
 
-type PlayTrackButtonProps = {
-  track: Track;
+type PlayPlaylistButtonProps = {
+  playlist_uri: string;
 };
 
-export default function PlayTrackButton({ track }: PlayTrackButtonProps) {
+export default function PlayPlaylistButton({
+  playlist_uri,
+}: PlayPlaylistButtonProps) {
   const { data: session } = useSession();
 
   return (
     <Button
-      isIconOnly
       radius='full'
       variant='light'
       onClick={() => {
-        playTrack(session as AuthSession, [track.uri]);
+        playPlaylist(session as AuthSession, playlist_uri);
       }}
     >
+      Play Tracklist
       <PlayCircle />
     </Button>
   );
