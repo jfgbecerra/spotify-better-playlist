d840eb4c45c6ae5cd176855139e04d0cc09ae25f
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2024-02-01 23:18:43 -0600

    Start of loading for sidebar

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,14 +1,18 @@
+import SidebarLoading from '@/components/Loading';
 import NavigationBar from '@/components/NavigationBar';
 import ContentPane from '@/components/content-pane/ContentPane';
 import Player from '@/components/player/Player';
 import Sidebar from '@/components/sidebar/Sidebar';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className='flex h-full flex-col overflow-hidden'>
       <NavigationBar />
       <div className='flex h-full flex-row gap-x-2 overflow-hidden px-2 pt-2'>
-        <Sidebar />
+        <Suspense fallback={<SidebarLoading />}>
+          <Sidebar />
+        </Suspense>
         <ContentPane />
       </div>
       <Player />
---
+++ src/components/Loading.tsx
@@ -0,0 +1,6 @@
+import { Skeleton } from '@nextui-org/react';
+
+export default function SidebarLoading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <Skeleton className='flex h-full w-96' />;
+}
